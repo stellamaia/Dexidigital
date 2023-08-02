20ed1d022d39c2039bbc6cd55bbc796f6e3e8274
Stella Maia <stellamaia97@hotmail.com>
2023-08-02 12:18:22 -0300

    Implementação do Firebase hosting

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,10 +8,14 @@ const firebaseConfig = {
     projectId: "dexidigital-59fa5",
     storageBucket: "dexidigital-59fa5.appspot.com",
     messagingSenderId: "690595741669",
-    appId: "1:690595741669:web:8fcf157abe1589ade3f90c"
+    appId: "1:690595741669:web:8fcf157abe1589ade3f90c",
+    measurementId: "G-4R29PPN3G6"
   };
 
 firebase.initializeApp(firebaseConfig);
 export const firebaseDb = firebase.firestore();
+// import 'firebase/analytics';
+// import { getAnalytics } from "firebase/analytics";
+// const analytics = getAnalytics(app);
 
 // export const firebaseAuth = firebase.auth();
