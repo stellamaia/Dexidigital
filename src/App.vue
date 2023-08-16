602fdd8530e6bbaba126841da9fcdbdc2298848c
Stella Maia <stellamaia97@hotmail.com>
2023-08-16 17:46:32 -0300

    App

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,15 @@
   <v-app>
     <div id="app">
       <router-view />
-      <WhatsappButton />
     </div>
   </v-app>
 </template>
 
 <script>
-import WhatsappButton from './components/WhatsappButton.vue';
 
 export default {
   name: 'App',
-  components: {
-    WhatsappButton
-  }
+ 
 };
 
 </script>
-
-
