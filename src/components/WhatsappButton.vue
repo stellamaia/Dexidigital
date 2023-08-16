74393e2abe377e752ca67b5b9b54de88398be1fa
Stella Maia <stellamaia97@hotmail.com>
2023-08-16 17:42:48 -0300

    Implementação do botao wpp

--- src/components/WhatsappButton.vue
+++ src/components/WhatsappButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="content-whatsapp">
         <v-btn class="whatsapp-button" @click="redirectToWhatsApp" ></v-btn>
     </div>
 </template>
@@ -18,7 +18,7 @@ export default {
 };
 </script>
 
-<style>
+<style >
 .whatsapp-button{
     position: fixed;
     top: auto !important;
