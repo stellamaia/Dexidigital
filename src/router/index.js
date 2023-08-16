47341a08d7492073e0a3aee1d2b4a0c16e5482da
Stella Maia <stellamaia97@hotmail.com>
2023-08-16 17:40:17 -0300

    Implementação de novas rotas

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,11 @@ import HomeView from '../views/HomeView.vue'
 import BlogView from '../views/BlogView.vue'
 import FormView from '../views/FormView.vue'
 import PostsView from '../views/PostsView.vue'
+import CreatePost from '../views/CreatePost.vue'
+import LoginView from '../views/LoginView.vue'
+// import SignView from '../views/SignView.vue'
+import EditPost from '../views/EditPost.vue'
+import PostEditPage from '../views/PostEditPage.vue'
 
 Vue.use(VueRouter)
 
@@ -27,6 +32,31 @@ const routes = [
     path: '/posts/:title',
     name: 'posts',
     component: PostsView
+  },
+  {
+    path: '/criar-post/',
+    name: 'create-Post',
+    component: CreatePost
+  },
+  {
+    path: '/entrar/',
+    name: 'login-view',
+    component: LoginView
+  },
+  // {
+  //   path: '/sign-view/',
+  //   name: 'sign-view',
+  //   component: SignView
+  // },
+  {
+    path: '/editar-post',
+    name: 'edit-post',
+    component: EditPost
+  },
+  {
+    path: '/pagina-de-edicao/:postId',
+    name: 'post-edit-page',
+    component: PostEditPage
   }
 ]
 
