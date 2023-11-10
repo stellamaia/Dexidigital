2e436d88cb41c6f2b7c2f329d8a75478c0e12fc3
Stella Maia <stellamaia97@hotmail.com>
2023-11-10 03:34:15 -0300

    Implementação de novas bibliotecas

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import VueGtag from 'vue-gtag'
+import VueI18n from 'vue-i18n'
+import ptBR from './locales/pt-BR.json'
+import en from './locales/en.json'
 
-import Vue2Editor from "vue2-editor"; 
+
+import Vue2Editor from "vue2-editor";
 Vue.use(Vue2Editor);
 
 Vue.use(VueGtag, {
@@ -13,6 +17,15 @@ Vue.use(VueGtag, {
     id: 'G-4R29PPN3G6'
   },
 });
+Vue.use(VueI18n)
+const i18n = new VueI18n({
+  locale: store.state.language, // Idioma padrão
+  fallbackLocale: 'pt-BR',
+  messages: {
+   'pt-BR': ptBR,
+    'en':en,// Adicione mais idiomas conforme necessário
+  }
+})
 
 import 'bootstrap/dist/css/bootstrap.css'
 import VueTheMask from 'vue-the-mask'
@@ -27,5 +40,6 @@ new Vue({
   router,
   store,
   vuetify,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
