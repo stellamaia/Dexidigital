ee1450279bfe43a226f786fedb53800bae1f40b9
Stella Maia <stellamaia97@hotmail.com>
2023-11-10 03:38:28 -0300

    Implementações de mutations

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        language: 'pt-BR'
+    },
+    mutations: {
+        setLanguage(state, language) {
+            state.language = language;
+        }
+    },
+    plugins: [createPersistedState()]
 })
